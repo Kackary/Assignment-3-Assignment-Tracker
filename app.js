08b2dbfb08e6f8fdc35cc6f16cd17b2bee6abb31
kackary <zachary.brienesse@ontariotechu.net>
2025-11-17 21:26:28 -0500

    navbar

--- app.js
+++ app.js
@@ -5,9 +5,14 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render("index");
+    res.render("index");
 });
 
+app.get('/assignments', (req, res) => {
+  res.render("assignments");
+});
+
+
 app.listen(3000, () => {
-  console.log("Server running at http://localhost:3000");
+    console.log("Server running at http://localhost:3000");
 });
